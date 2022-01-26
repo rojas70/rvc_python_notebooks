7fbb0f70546a76aac2af72bd35e361889cd6233d
rojas70@gmail.com <rojas70@gmail.com>
2022-01-26 13:58:27 +0800

    multiple updates

--- scrips/7.4_2link_robot_velocity.py
+++ scrips/7.4_2link_robot_velocity.py
@@ -12,17 +12,4 @@ p2 = rtb.models.DH.Planar2()
 p2.q = [0.1, 0.2]
 
 # Plot and include the Velocity Ellipsoid
-#p2.plot(p2.q, vellipse=True)
-
-from roboticstoolbox import ETS as ets
-# Links 
-a1=1
-a2=1
-
-# Angles
-q1=0
-q2 = np.pi/2
-
-e = ets.rz(q1)*ets.tx(a1)*ets.rz(q2)*ets.tx(a2)
-print(e.eval( [q1, q2] ))
-e.plot([q1,q2],vellipse=True)
+p2.plot(p2.q, vellipse=True)
